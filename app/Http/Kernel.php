f56e8f0ecb510fd02e78d0bd661755f925b3858c
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2021-12-05 22:36:58 +0530

    roles and permission middleware registered

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,12 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        /**
+         * Permissions Middleware
+         */
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
     ];
 }
