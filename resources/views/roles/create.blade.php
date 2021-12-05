9d23770ffeef305f2f6cf160aec20ae667645b54
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2021-12-05 22:39:30 +0530

    ui improvement

--- resources/views/roles/create.blade.php
+++ resources/views/roles/create.blade.php
@@ -3,27 +3,32 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container card p-4">
         <form action="{{ route('admin.roles.store') }}" method="post">
             @csrf
             <div class="row justify-content-end">
-                <button type="submit" class="col-2 btn btn-primary">Submit</button>
+                <button type="submit" class="col-12 col-md-3 btn btn-primary">Submit</button>
             </div>
             <div class="row">
                 <div class="col-12 col-md-4">
                     <div class="mb-3">
                         <label for="name">Role Name</label>
-                        <input id="name" name="name" type="text" class="form-control" placeholder="e.g. Manager"/>
+                        <input id="name" name="name" type="text" class="form-control" placeholder="e.g. Manager" />
                     </div>
                 </div>
-                <h3>Permissions</h3>
+            </div>
+            <h3>Permissions</h3>
+            <br>
+            <div class="row">
                 @foreach ($permissions as $permission)
-                <div class="col-12 col-md-4">
-                    <div class="form-check mb-3">
-                        <input id="permissions" name="permissions[]" type="checkbox" class="form-check-input" value="{{ $permission->id }}" />
-                        <label id="permissions_label" class="form-check-label" for="permissions">{{ $permission->name }}</label>
+                    <div class="col-12 col-md-4">
+                        <div class="form-check mb-3">
+                            <input id="permissions" name="permissions[]" type="checkbox" class="form-check-input"
+                                value="{{ $permission->id }}" />
+                            <label id="permissions_label" class="form-check-label"
+                                for="permissions">{{ $permission->name }}</label>
+                        </div>
                     </div>
-                </div>
                 @endforeach
             </div>
         </form>
