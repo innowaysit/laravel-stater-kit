351ad60ea5c00b45fecac4c73e02c55b5140a40d
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2021-12-08 20:33:38 +0530

    breadcrumbs

--- resources/views/permissions/create.blade.php
+++ resources/views/permissions/create.blade.php
@@ -3,17 +3,30 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="{{ route('admin.users.index') }}">Permissions</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page">New Permission</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="card m-2 p-4">
         <form action="{{ route('admin.permissions.store') }}" method="post">
             @csrf
             <div class="row justify-content-end">
-                <button type="submit" class="col-2 btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Submit</button>
             </div>
             <div class="row">
                 <div class="col-12 col-md-4">
                     <div class="mb-3">
                         <label for="name">Permission Name</label>
-                        <input id="name" name="name" type="text" class="form-control" placeholder="e.g. blog-write"/>
+                        <input id="name" name="name" type="text" class="form-control" placeholder="e.g. blog-write" />
                     </div>
                 </div>
             </div>
--- resources/views/permissions/index.blade.php
+++ resources/views/permissions/index.blade.php
@@ -3,34 +3,32 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <a href="{{ route('admin.permissions.create') }}" class="btn btn-primary btn-sm">New Permission</a>
+    <div class="row">
+        <div class="col-12">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb mb-0">
+                    <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">Dashboard</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Permissions</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+
+    <div class="card m-2 p-4">
+        <div class="row justify-content-end">
+            <div class="mx-3">
+                <a href="{{ route('admin.permissions.create') }}" class="btn btn-primary">New Permission</a>
             </div>
         </div>
-        <div class="row">
-            <div class="col-12 table-responsive">
-                <table class="table table-hover table-light">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Permission</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse($permissions as $permission)
-                            <tr>
-                                <td scope="row"></td>
-                                <td>{{ $permission->name }}</td>
-                            </tr>
-                        @empty
-                            <tr>
-                                <td colspan="2">No permissions found</td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+        <div class="row my-2">
+            <div class="col-12">
+                @forelse($permissions as $permission)
+                    <div class="btn btn-outline-primary">{{ $permission->name }}</div>
+                @empty
+                    <div class="alert alert-light" role="alert">
+                        No permissions found
+                    </div>
+                @endforelse
             </div>
         </div>
     </div>
