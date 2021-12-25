e5d55392bc0a3098000af815fbc03538e3141409
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2021-12-25 16:27:30 +0530

    footer updated

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -3,15 +3,14 @@
         <div class="row">
             <div class="col-lg-6">
                 <ul class="list-inline mb-0">
-                    <li class="list-inline-item"><a href="../backend/privacy-policy.html">Privacy Policy</a></li>
-                    <li class="list-inline-item"><a href="../backend/terms-of-service.html">Terms of Use</a></li>
+                    <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+                    <li class="list-inline-item"><a href="#">Terms of Use</a></li>
                 </ul>
             </div>
             <div class="col-lg-6 text-right">
                 <script>
                     document.write(new Date().getFullYear())
-                </script>©</span> <a href="#"
-                    class="">Webkit</a>.
+                </script>©</span> <a href="#" class="">{{ config('app.name') }}</a>.
             </div>
         </div>
     </div>
