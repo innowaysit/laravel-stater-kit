031c226f81bb1ea433f63e15496ad5812be5d87f
Dalpat Singh <dalpatsinghpurohit@gmail.com>
2022-09-21 12:35:01 +0530

    blank page added

---
+++ resources/views/blank.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+
+@section('content')
+    {{-- Your content here --}}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::view('/', 'welcome');
+Route::view('/blank', 'blank');
 
 Auth::routes();
 
